84634dd50d2594ad82947bc3f64568bc2d480c04
韩升 <857149061@qq.com>
2023-01-03 12:00:26 +0800

    add whitelist

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -2,25 +2,26 @@
 pragma solidity ^0.8.11;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./GetFee.sol";
 import "./Game.sol";
 import "./Hero.sol";
 import "./NFT.sol";
 
-contract Box is Ownable{
+contract Box is Ownable, ReentrancyGuard{
     constructor() {
     }
-    IERC20 public erc20 = IERC20(0x0a2231B33152d059454FF43F616E4434Afb6Cc64);
     mapping(uint256=>address) _boxByUser;
     mapping(address=>uint256[]) _userBoxs;
     uint256 _boxId = 0;
     uint256 _boxPrice=100*10**18;
     Game public  _game;
     Hero _hero = Hero(0xCDdB3Df2ecEa4A23ddf36644B82920677be3FFB2);
-    GetFee public getFeeRate = GetFee(0x94329c959B84B373bca334EE171192863beF07f0);
     NFT public _nft = NFT(0x03960BF2C1074c915a86618433f1E580C3cbfA59);
     uint256 _lockTime = 12*3600;
     uint256 _temNum=5;
+    mapping(address => bool) public whiteList;
+    address public bank;
 
 
     struct monsterInfo{
@@ -33,18 +34,31 @@ contract Box is Ownable{
      
     event BuyBox(uint32 index,uint256 price,address sender);
     event OpenBox(uint256 indexed rarity,uint256 indexed tokenId,uint256 monsterId,address sender);
+    event AddWhiteList(address indexed account);
+
+    function addWhiteList(address account) public onlyOwner {
+        whiteList[account] = true;
+        emit AddWhiteList(account);
+    }
+
+    function setBank(address _bank) public onlyOwner {
+        bank = _bank;
+    }
     
-    function buyBox()  public {
-        uint256 amount=100*10**18  ;
-        amount = getFeeRate.getUsdtPrice1(_boxPrice);
-        
-        require(erc20.allowance(msg.sender, address(this)) >= amount, "allowance amount not enough");
-        erc20.transferFrom(msg.sender, address(this), amount);
+    function buyBox()  public nonReentrant payable{
+        payable(bank).transfer(_boxPrice);
         _boxByUser[_boxId]= msg.sender;
         _userBoxs[msg.sender].push(_boxId);
-        _boxId +=1;
+        _boxId += 1;
+
         _hero.initHeroEq(msg.sender);
         emit BuyBox(1,uint256(amount),msg.sender);
+    }
+
+    function buyBoxBatch(uint256 amount) public payable {
+        for(uint256 i = 0; i < amount; ++i) {
+            buyBox();
+        }
     }
     
     function openBox(uint32 _index)  public {
@@ -69,6 +83,12 @@ contract Box is Ownable{
         emit OpenBox(nftKindId,tokenId,monsterId,msg.sender);
     }
 
+    function openBoxBatch(uint32[] memory indexs) public {
+        for(uint256 i = 0; i < indexs.length; ++i) {
+            openBox(indexs[i]);
+        }
+    }
+
     function getUserBoxs(address sender) view public returns(uint256[] memory){
         return _userBoxs[sender];
     }
@@ -78,8 +98,7 @@ contract Box is Ownable{
     }
 
     function getBoxPrice() public view returns(uint256 amounts){
-        amounts = getFeeRate.getUsdtPrice1(_boxPrice);
-        return amounts;
+        amounts = _boxPrice;
     }
 
     function withdrawal(address tokenAddress,address from,address to,uint256 amount) public onlyOwner{
@@ -88,14 +107,6 @@ contract Box is Ownable{
 
     function setGame(address payable _gameAddress) public onlyOwner{
         _game = Game(_gameAddress);
-    }
-
-    function setRouter(address _feeAddress) public onlyOwner{
-        getFeeRate = GetFee(_feeAddress);
-    }
-
-    function setERC20Addr(address _tokenAddress)public onlyOwner{
-        erc20 = IERC20(_tokenAddress);
     }
 
     function setHero(address _tokenAddress)public onlyOwner{
